39e15d6d841f4243c1739837886e3e0568f107fb
alinuk1992 <ali.nourikhah@gmail.com>
2022-11-10 18:04:55 +0330

    initial commit

---
+++ src/routes/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import CoinSearch from "../components/CoinSearch";
+import Trending from "../components/Trending";
+
+const Home = ({ coins }) => {
+  return (
+    <>
+      <CoinSearch coins={coins} />
+      <Trending />
+    </>
+  );
+};
+
+export default Home;
